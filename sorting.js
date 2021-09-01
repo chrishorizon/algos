d07daf99ad51ca6da8cdc22110fb94b4faa0f58a
Chris <christhehorizon@gmail.com>
2021-09-01 10:10:29 -0700

    MergeSort algo

--- sorting.js
+++ sorting.js
@@ -49,7 +49,9 @@ const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  * @param {Array<number>} nums
  * @returns {Array<number>} The given array after being sorted.
  */
-function selectionSort(nums) {}
+function selectionSort(nums) {
+
+}
 
 
 /* 
@@ -102,3 +104,84 @@ function insertionSort(nums) {
     }
     return nums;
 }
+
+// Sorting numbers in an array
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a-b});
+
+console.log(points)
+
+/* 
+  Merge / Stable sort.
+  Visualization:
+  https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+  Time Complexity
+    - Best case: O(n log(n)) linearithmic.
+    - Average case: O(n log(n)) linearithmic.
+    - Worst case: O(n log(n)) linearithmic.
+  Space: O(n) linear
+  steps:
+    1. create a merge function to merge two already sorted arrays into a single
+        sorted array.
+      - you do NOT need to work in place, ok to use a new array
+    2. create mergeSort function to sort the provided array
+      - split the array in half and recursively merge the halves using the
+          previously created merge function.
+      - splitting of arrays stops when array can no longer be split.
+      - an array of 1 item is by definition sorted, so two arrays of 1 item
+          can therefore be merged into a sorted array.
+*/
+
+// merge
+const sortedA1 = [];
+const sortedB1 = [];
+const expectedMerge1 = [];
+
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [3];
+const sortedB3 = [2, 3, 4, 7];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+const sortedA4 = [1, 2, 4, 5, 6, 9];
+const sortedB4 = [3, 7, 8, 10];
+const expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/* Efficiently merges two already sorted arrays into a new sorted array. */
+function merge(left, right) {
+    const arr = [];
+    while(left.length && right.length){
+        arr.push(left[0] > right[0] ? right.shift() : left.shift())
+    }
+    // while (left.length) {
+    //     arr.push(left.shift())
+    // }
+    // while (right.length) {
+    //     arr.push(right.shift())
+    // }
+    // return arr;
+    return [...arr, ...left, ...right]
+}
+// console.log(merge(sortedA3, sortedB3))
+
+// mergeSort
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expectedSort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function mergeSort(nums) {
+    //base case
+    if (nums.length == 1) {
+        return nums;
+    }
+    // slice left and right
+    var left = mergeSort(nums.slice(0, nums.length / 2));
+    var right = mergeSort(nums.slice(nums.length / 2));
+    // forward engeering and recursion
+    return merge(left, right);
+}
+
+console.log(mergeSort(numsRandomOrder))
