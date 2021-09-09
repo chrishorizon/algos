cfef527628e14b830983c819d1090587401dc8d6
Chris <christhehorizon@gmail.com>
2021-09-09 11:06:32 -0700

    Find conseq. Sums Algo

--- arrays.js
+++ arrays.js
@@ -153,8 +153,6 @@ Venn Diagram Visualization:
 * Both given sets are multisets in any order (contain dupes).
 * @returns {Array<number>} The union of the given sets but excluding the shared
 * values (union without intersection).
-* i.e., if the element is in one array and NOT the other, it should be
-* included in the return.
 */
 
 const test1NumsA = [1, 2];
@@ -187,3 +185,89 @@ function symmetricDifferences(numsA, numsB) {
 }
 
 console.log(symmetricDifferences(test3NumsA, test3NumsB))
+
+/* ----------------------------------------------------------------------
+Given an array of ints, find all the non-consecutive integers
+A number is non-consecutive if it is NOT exactly 1 larger than the previous element.
+The first element is never considered non-consecutive.
+Return an array of objects where each object contains the number itself
+and it's index.
+*/
+
+const nums1 = [1, 2, 3, 4, 6, 7, 8, 10];
+const expected1 = [
+    { i: 4, n: 6 },
+    { i: 7, n: 10 },
+];
+
+function allNonConsecutive(sortedNums) {
+    let newArr = [];
+    for(let i = 1; i < sortedNums.length; i++){
+        if(sortedNums[i] - sortedNums[i-1] !== 1){
+            newArr.push({
+                "i": i,
+                "n": sortedNums[i]
+            })
+        }
+    }
+    return newArr;
+}
+
+console.log(allNonConsecutive(nums1))
+
+
+/* 
+You are given a list of integers. Find all the consecutive sets of 
+integers that adds up to the sum passed in as one of the inputs.
+    * @param {Array<number>} nums Unordered nums.
+    * @param {number} targetSum
+    * @returns {Array<Array<number>>} 2d array where each nested array is a set of
+    *    consecutive numbers that add up to the given targetSum. Consecutive in
+    *    this context means the numbers whose indexes are one after the other
+    *    only.
+*/
+
+const nums1 = [2, 5, 3, 6, 7, 23, 12];
+const sum1 = 16;
+const expected1 = [
+    [2, 5, 3, 6],
+    [3, 6, 7],
+];
+
+// Bonus:
+const nums2 = [2, 5, 3, 6, 7, 0, 0, 23, 12];
+const sum2 = 16;
+const expected2 = [
+    [2, 5, 3, 6],
+    [3, 6, 7],
+    [3, 6, 7, 0],
+    [3, 6, 7, 0, 0],
+];
+
+// Bonus:
+const nums3 = [-2, -5, -3, -6, -7, -0, -0, -23, -12];
+const sum3 = -16;
+const expected3 = [
+    [-2, -5, -3, -6],
+    [-3, -6, -7],
+    [-3, -6, -7, -0],
+    [-3, -6, -7, -0, -0],
+];
+
+function findConsqSums(nums, targetSum) {
+    let results = []
+    for (let i = 0; i < nums.length; i++) {
+        sum = 0;
+        idx = i;
+        while ((targetSum >= 0 && sum <= targetSum) || (targetSum < 0 && sum >= targetSum)) {
+            sum += nums[idx];
+            if (sum === targetSum) {
+                results.push(nums.slice(i, idx + 1));
+            }
+            idx++;
+        }
+    }
+    return results;
+}
+
+console.log(findConsqSums(nums2, 16))
