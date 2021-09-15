6b3ba75e79e79a05d1c0496c72b6f1a582ec5539
Chris <christhehorizon@gmail.com>
2021-09-15 12:07:24 -0700

    Filter By Key algo

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-/* 
+/*  ENTRIES *
 Recreate Object.entries method
 Given an object,
 return an array of arrays of the object's key value pairs, where each key value pair is a 2 item array
@@ -40,9 +40,8 @@ function entries(obj) {
 
 console.log(entries(obj2))
 
-/*****************************************************************************/
 
-/* 
+/*     INSERT 
 Given a table name string and an object whose key value pairs represent column names and values for the columns
 return a SQL insert statement string
 Tip: string interpolation (using back ticks, the key to the left of num 1 key) make it easy to add variables into a string or to add quotations without needing to escape them.
@@ -86,6 +85,7 @@ function insert(tableName, columnValuePairs) {
 console.log(insert(table, insertData1))
 
 /* **********************************************************************************
+    * FIND OBJECTS* 
 Given a search criteria object whose values will only be
 primitives (ints, strings, booleans) and a list of objects.
 return any object that matches all the key value pairs in the search
@@ -129,6 +129,24 @@ function findObjects(criteria, collection) {
     return result
 }
 
+// Version 2
+function findObjects1(criteria, collection) {
+    var toReturn = [];
+    for (var i=0; i<collection.length; i++) {
+        var toAdd = true;
+        for (const [key, value] of Object.entries(criteria)) {
+            if (collection[i][key] !== value) {
+                toAdd = false;
+                break;
+            }
+        }
+        if (toAdd) {
+            toReturn.push(collection[i]);
+        }
+    }
+    return toReturn;
+}
+
 // Functional Programming solution
 const findObjects2 = (criteria, collection) => {
     collection.filter((student) => {
@@ -138,9 +156,7 @@ const findObjects2 = (criteria, collection) => {
 console.log(findObjects2(searchCriteria1, items))
 
 
-/*****************************************************************************/
-
-/* 
+/*    * FIND BY AND UPDATE *
 Given an id, an object that has keys with corresponding updated values, and an array of objects
 Find the object by "id" key that matches the given id value and then update that object's
 keys with the provided new values.
@@ -223,6 +239,241 @@ function findByIdAndUpdate(id, updatedVals, collection) {
     }
     return person
 }
+
+// Version 2
+function findByIdAndUpdate2(id, updatedVals, collection) {
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i].id === id) {
+            for (const [key, value] of Object.entries(updatedVals)) {
+                if (collection[i][key]) {
+                    collection[i][key] = value;
+                }
+            }
+            return collection[i];
+        }
+    }
+}
+
 console.log(findByIdAndUpdate(id1, updateData1, students))
 console.log(findByIdAndUpdate(id2, updateData2, students))
 console.log(findByIdAndUpdate(id3, updateData3, students))
+
+/* **************************************************************************************
+
+    * FILTER BY KEY *
+Given an array of objects, a searchFor string, and searchBy key that exists in the object
+return a new array of only those objects whose value for the given key starts with the given search string
+You can assume the key will exist on the object and the value of that key will be a string
+Bonus: make the search case insensitive
+Bonus: re-write it with functional programming in mind, using built in methods
+Bonus: allow the search method to be provided as a parameter, e.g., string methods: includes, startsWith, endsWith
+    - you can assume the searchMethod will be valid
+*//**
+ * @param {Array<Object>} items The items to be filtered.
+ * @param {string} searchBy The key to search by.
+ * @param {string} searchFor The value of the given key to search for.
+ * @returns {Array<Objects>} The matched items.
+ */
+
+const people = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Eddy",
+        lastName: "Lee",
+    },
+    {
+        firstName: "John",
+        lastName: "Fawn",
+    },
+    {
+        firstName: "Edward",
+        lastName: "Kim",
+    },
+];
+
+const searchFor1 = "Jo";
+const searchBy1 = "firstName";
+const expected1 = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "John",
+        lastName: "Fawn",
+    },
+];
+
+const searchFor2 = "ohn";
+const searchBy2 = "firstName";
+const expected2 = [];
+// Explanation: "John" contains "ohn", it does not start with "ohn"
+
+const searchFor3 = "Do";
+const searchBy3 = "lastName";
+const expected3 = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+    },
+];
+
+// Bonus
+const searchFor4 = "E";
+const searchBy4 = "lastName";
+const searchMethod4 = "includes";
+const expected4 = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+    },
+    {
+        firstName: "Eddy",
+        lastName: "Lee",
+    },
+];
+
+function filterByKey(items, searchFor, searchBy) {
+    let name =[];
+    for(let i = 0; i < items.length -1; i++){
+        if(items[i][searchBy].includes(searchFor)){
+            name.push(items[i])
+        }
+    }
+    return name;
+}
+
+// Version 2
+function filterByKey2(items, searchFor, searchBy, searchMethod) {
+    switch (searchMethod) {
+        case 'startsWith':
+            return items.filter((item) => item[searchBy].toLowerCase().startsWith(searchFor.toLowerCase()));
+        case 'includes':
+            return items.filter((item) => item[searchBy].toLowerCase().includes(searchFor.toLowerCase()));
+        case 'endsWith':
+            return items.filter((item) => item[searchBy].toLowerCase().endsWith(searchFor.toLowerCase()));
+    }
+}
+
+// Version 3
+function filterByKey3(items, searchFor, searchBy) {
+    return items.filter((item) => item[searchBy].toLowerCase().startsWith(searchFor.toLowerCase()));
+}
+
+console.log(filterByKey(people, searchFor1, searchBy1))
+
+const filteredList = filterByKey(people, searchFor3, searchBy3)
+console.log(filteredList)
+
+module.exports = { filterByKey };
+
+/* 
+Given an array of person objects with the following keys:
+    return a new array of the names of people (not friends) who are at high risk of infection
+    A person is at high risk of catching the virus if they meet all the below criteria:
+    1. not practicing social distancing
+    2. have a friend who is not practicing social distancing whom hasCovid
+    Bonus: after solving it, make a 2nd solution to practice functional programming with built in methods
+*/
+
+const friend1 = {
+    firstName: "Friend",
+    lastName: "One",
+    isSocialDistancing: false,
+    hasCovid: true,
+};
+
+const friend2 = {
+    firstName: "Friend",
+    lastName: "Two",
+    isSocialDistancing: false,
+    hasCovid: true,
+};
+
+const friend3 = {
+    firstName: "Friend",
+    lastName: "Three",
+    isSocialDistancing: false,
+    hasCovid: false,
+};
+
+const people = [
+    {
+        firstName: "Person",
+        lastName: "One",
+        isSocialDistancing: false,
+        friends: [friend2, friend3],
+    },
+    {
+        firstName: "Person",
+        lastName: "Two",
+        isSocialDistancing: true,
+        friends: [friend2, friend1],
+    },
+    {
+        firstName: "Person",
+        lastName: "Three",
+        isSocialDistancing: false,
+        friends: [friend2, friend1],
+    },
+];
+
+const expected = ["Person One", "Person Three"];
+
+/**
+ * @typedef {Object} Friend
+ * @property {string} firstName
+ * @property {string} lastName
+ * @property {boolean} isSocialDistancing
+ * @property {boolean} hasCovid
+ *
+ * @typedef {Object} Person
+ * @property {Array<Friend>} friends
+ * @property {string} firstName
+ * @property {string} lastName
+ * @property {boolean} isSocialDistancing
+ */
+
+/**
+ * Finds the people who are at risk of contracting Covid.
+ * - Time O(?).
+ * - Space O(?).
+ * @param {Array<Person>} persons
+ * @returns {Array<string>} An array of Person full names for those people who
+ *    are at risk. A Person is at risk if:
+ *    1. not practicing social distancing.
+ *    2. have a friend who is not practicing social distancing whom hasCovid.
+ */
+
+function coronaVirusAtRisk(persons) {
+    var atRiskPeople = [];
+    persons.forEach((person) => {
+        if (person.isSocialDistancing === false) {
+            var atRisk = false;
+            person.friends.forEach((friend) => {
+                if (friend.isSocialDistancing === false && friend.hasCovid === true) {
+                    atRisk = true;
+                }
+            })
+            if (atRisk === true) {
+                atRiskPeople.push((person.firstName + " " + person.lastName));
+            }
+        }
+    })
+    return atRiskPeople;
+}
