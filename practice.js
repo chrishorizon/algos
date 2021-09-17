dbf59801bf82e36e57b2361888655270526410b2
Chris <christhehorizon@gmail.com>
2021-09-17 16:42:06 -0700

    rev an array

--- practice.js
+++ practice.js
@@ -63,3 +63,16 @@ for(var i = 1; i <= 100; i++){
     (i % 15 == 0 ? console.log("Fizzbuzz") : console.log(i));
 }
 
+var testOne = ["a", "b", "c", "d", "e"]
+
+function revArr(arr){
+    let newArr = [];
+    for(let i = arr.length -1; i >= 0; i--){
+        newArr.push(arr[i])
+    }
+    return newArr;
+}
+
+var revTestOne = revArr(testOne)
+
+console.log(revTestOne)
