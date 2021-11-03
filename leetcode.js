4b11078a8044a4c842783e2d7500a7de0cb01c34
Chris <christhehorizon@gmail.com>
2021-11-03 08:55:06 -0700

    LeetCode TwoSum algo declared

---
+++ leetcode.js
@@ -0,0 +1,6 @@
+// Two Sum
+// given an array of integers nums and an integer target, return indices of the
+// two integers such that they add up to target.
+function twoSum(nums, target){
+    
+}
