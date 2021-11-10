99f1957197cf31248f159252c13cdac6458b3c2e
Chris <christhehorizon@gmail.com>
2021-11-09 23:16:33 -0800

    Leetcode containerWithMostWater algo solution

--- leetcode.js
+++ leetcode.js
@@ -84,5 +84,18 @@ console.log(maxProfit(prices));
  */
 
 var maxArea = function(height) {
-    
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length -1;
+
+    while(left < right){
+        let min = (right - left) * Math.min(height[left], height[right]);
+        maxArea = Math.max(min, maxArea);
+        if(height[left] < height[right]){
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
 };
