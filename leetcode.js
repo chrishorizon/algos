eba2e90c7cb336f866b269d960e7fc5cb58bf42e
Chris <christhehorizon@gmail.com>
2021-11-11 16:56:35 -0800

    Leetcode productExceptSelf algo initialized:

--- leetcode.js
+++ leetcode.js
@@ -162,5 +162,23 @@ var threeSum = function(nums) {
     }
     return res;
 };
-
 console.log(threeSum(num));
+
+
+// ===== Product of Array Except Self =================
+// Given an integer array nums, return an array answer such that answer[i] is
+// equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var productExceptSelf = function(nums) {
+    // edge cases
+    if(nums == null || nums.length == 1) return null;
+
+    
+
+};
