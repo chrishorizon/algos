6a8c141ba4daa26355eea48c87100a384e30ad73
Chris <christhehorizon@gmail.com>
2021-11-15 10:09:22 -0800

    Leetcode searchRotatedArray algo initialized

--- leetcode.js
+++ leetcode.js
@@ -266,3 +266,20 @@ var findMin = function(nums) {
     return nums[left];
 };
 console.log(findMin(numArr));
+
+
+// ======= Search in Rotated Sorted Array ==============
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+let numsArr = [4,5,6,7,0,1,2];
+
+var search = function(nums, target) {
+    
+};
