29ce421e17dec785cde9c6d22e83dd982069dae0
Chris <christhehorizon@gmail.com>
2021-12-04 20:25:36 -0800

    bubbleSort pseudocode

--- sort.js
+++ sort.js
@@ -3,5 +3,9 @@
 const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
 function bubbleSort(arr) {
-    
+    // loop through i
+        // nested loop with j
+            // if i is greater than j, return swap i with j
+        
+    // console log arr
 }
