2830c19a6438fe7fa25325dc5e5a483f56ed6ffe
Chris <ch4562@att.com>
2022-11-17 20:37:10 -0800

    3Sum leetcode solution

--- leetcode_practice.js
+++ leetcode_practice.js
@@ -14,7 +14,7 @@ function reverseString(str) {
 // console.log(reverseString(randomStr));
 
 
-/* TWO SUM
+/* **** TWO SUM ****
 Given an array of integers nums and an integer target, 
 return indices of the two numbers such that they add up to target.
 */
@@ -38,12 +38,50 @@ function twoSum(arr, target) {
     }
     return result;
 }
-// console.log(twoSum(numsArr, 9));
+// console.log(twoSum(numsArr, 17));
 
 
-// 3Sum
-let num = [-1,0,1,2,-1,-4];
-let sorted = [-4, -1, -1, 0, 1, 2]
+/* **** 3SUM ****
+* Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] that == 0.
+*/
+let num = [-1,0,1,2,-1,-4]; // sorted [-4, -1, -1, 0, 1, 2]
+
+function threeSum(arr) {
+    // declare variable and assign an empty array
+    let result = [];
+    // sort array
+    arr.sort((a, b) => a - b);
+    // EDGE CASE - if arr is null or less than 3, return
+    if (arr == null || arr.length < 3) return result;
+
+    let right = arr.length-1;
+
+    // for loop through array
+    for (let i = 0; i < arr.length - 2; i++) {
+        // if current idx equals previous idx, continue
+        if (arr[i] === arr[i-1]) continue;
+        let left = i + 1;
+        
+        while (left < right) {
+            // Declare variable with sum of i, j, k index's
+            let sum = arr[i] + arr[left] + arr[right];
+            if (sum === 0) {
+                result.push([arr[i], arr[left], arr[right]]);
+                while (arr[left] === arr[left + 1]) left++;
+                while (arr[right] === arr[right - 1]) right--;
+                left++;
+                right--;
+            }
+            // if sum var is less than 0, move left. If greater, than decrement right
+            else if ( sum < 0) {
+                left++;
+            } else { right--; }
+        }
+    }
+    return result;
+}
+// console.log(threeSum(num));
+
 
 // Container With Most Water
 var waterLevel = [3, 5, 1, 10, 6, 6];
